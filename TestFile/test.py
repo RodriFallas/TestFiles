67dddf0fc07dd9540b214a21347d68cfba4606e4
RodrigoFallas <RodrigoFallas@CR-RFALLAS>
2023-07-28 12:02:36 -0600

    TC1

--- TestFile/test.py
+++ TestFile/test.py
@@ -2,15 +2,17 @@ import os
 import pandas as pd
 
 def guardar_hello_world_en_excel():
+    palabra = "Hello World"
+    
     # Crear un DataFrame con la palabra "Hello World"
-    df = pd.DataFrame({'Palabra': ['Hello World']})
+    df = pd.DataFrame({'Palabra': [palabra]})
 
-    # Verificamos si la carpeta "test" existe, si no, la creamos
-    if not os.path.exists("test"):
-        os.makedirs("test")
+    # Verificamos si la carpeta "C:\Test" existe, si no, la creamos
+    if not os.path.exists("C:\Test"):
+        os.makedirs("C:\Test")
 
-    # Guardar el DataFrame en un archivo Excel en la carpeta "test"
-    archivo_excel = "test/Hello_World.xlsx"
+    # Guardar el DataFrame en un archivo Excel en la carpeta "C:\Test"
+    archivo_excel = f"C:\\Test\\Hello_World.xlsx"
     df.to_excel(archivo_excel, index=False)
     print(f"El archivo '{archivo_excel}' ha sido creado y la palabra 'Hello World' ha sido guardada en él.")
 
