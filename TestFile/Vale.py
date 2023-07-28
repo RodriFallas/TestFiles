29cc9ec1fd06c5803f0762490c8667d345a665d6
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-07-28 13:31:03 -0600

    fileVale

---
+++ TestFile/Vale.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+def guardar_hello_world_en_excel():
+    palabra = "Hello World"
+    
+    # Crear un DataFrame con la palabra "Hello World"
+    df = pd.DataFrame({'Palabra': [palabra]})
+
+    # Verificamos si la carpeta "/ruta/del/workspace/mi_proyecto/C:\Test" existe, si no, la creamos
+    if not os.path.exists("/ruta/del/workspace/mi_proyecto/C:\\Test"):
+        os.makedirs("/ruta/del/workspace/mi_proyecto/C:\\Test")
+
+    # Guardar el DataFrame en un archivo Excel en la carpeta "/ruta/del/workspace/mi_proyecto/C:\Test"
+    archivo_excel = "/ruta/del/workspace/mi_proyecto/C:\\Test\\Hello_World.xlsx"
+    df.to_excel(archivo_excel, index=False)
+    print(f"El archivo '{archivo_excel}' ha sido creado y la palabra 'Hello World' ha sido guardada en él.")
+
+if __name__ == "__main__":
+    guardar_hello_world_en_excel()
