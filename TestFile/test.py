a835a2bf5bf30e1b69a00de3c901ca85714e7815
RodrigoFallas <RodrigoFallas@CR-RFALLAS>
2023-07-31 11:03:51 -0600

    Updatecode

--- TestFile/test.py
+++ TestFile/test.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from git import Repo
 
 def guardar_hello_world_en_excel():
     palabra = "Hello World"
@@ -14,6 +15,20 @@ def guardar_hello_world_en_excel():
     archivo_excel = os.path.join(directorio_trabajo, "Hello_World.xlsx")
     df.to_excel(archivo_excel, index=False)
     print(f"El archivo '{archivo_excel}' ha sido creado y la palabra 'Hello World' ha sido guardada en él.")
+
+    # Agregar el archivo Excel al área de preparación de Git
+    repo = Repo(directorio_trabajo)
+    repo.git.add(archivo_excel)
+
+    # Hacer el commit con un mensaje
+    commit_message = "Agregando archivo Excel generado por Jenkins"
+    repo.index.commit(commit_message)
+
+    # Hacer push al repositorio remoto en GitHub
+    remote_origin = repo.remote(name="origin")
+    remote_origin.push()
+
+    print(f"Se ha hecho el commit y el push al repositorio remoto en GitHub.")
 
 if __name__ == "__main__":
     guardar_hello_world_en_excel()
