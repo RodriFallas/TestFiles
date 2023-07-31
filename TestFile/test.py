f4ce6942effcf327baacc48d11aa461bf49b43dd
RodrigoFallas <RodrigoFallas@CR-RFALLAS>
2023-07-31 10:31:55 -0600

    Updatepath

--- TestFile/test.py
+++ TestFile/test.py
@@ -7,10 +7,11 @@ def guardar_hello_world_en_excel():
     # Crear un DataFrame con la palabra "Hello World"
     df = pd.DataFrame({'Palabra': [palabra]})
 
-    # Especificar una ruta absoluta para el archivo Excel
-    archivo_excel = "C:\Test\Hello_World.xlsx"
+    # Obtenemos la ruta del directorio de trabajo actual de Jenkins
+    directorio_trabajo = os.environ['WORKSPACE']
 
-    # Guardar el DataFrame en un archivo Excel
+    # Guardar el DataFrame en un archivo Excel en el directorio de trabajo de Jenkins
+    archivo_excel = os.path.join(directorio_trabajo, "Hello_World.xlsx")
     df.to_excel(archivo_excel, index=False)
     print(f"El archivo '{archivo_excel}' ha sido creado y la palabra 'Hello World' ha sido guardada en él.")
 
